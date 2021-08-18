864d182a4736d286cc8c533b6fca82e5032c9450
root <kcelestinomaria127@gmail.com>
2021-08-18 11:09:35 +0000

    js frontend

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+
+(async () => {
+  const startingBalance = stdlib.parseCurrency(10);
+  const accAlice = await stdlib.newTestAccount(startingBalance);
+  const accBob = await stdlib.newTestAccount(startingBalance);
+
+  const ctcAlice = accAlice.deploy(backend);
+  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+
+  await Promise.all([
+    backend.ALice(ctcAlice, {
+      // implement Alice's interact object here
+    }),
+    backend.Bob(ctcBob, {
+      // implement Bob's interact object here
+    }),
+  ]);
+})();
